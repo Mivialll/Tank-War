8d7955986d550ddcc28b4d493912ea2d8192d5f6
Linghan Li <32752716+Mivialll@users.noreply.github.com>
2017-12-10 21:37:31 -0500

    Update CameraControl.cs

--- CameraControl.cs
+++ CameraControl.cs
@@ -21,19 +21,17 @@ public class CameraControl: MonoBehaviour
 
 
     private void FixedUpdate (){
-        // Move the camera towards a desired position.
+  
         Move ();
 
-        // Change the size of the camera based.
         Zoom ();
     }
 
 
     private void Move (){
-        // Find the average position of the targets.
+
         FindAveragePosition ();
 
-        // Smoothly transition to that position.
         transform.position = Vector3.SmoothDamp(transform.position, m_DesiredPosition, ref m_MoveVelocity, m_DampTime);
     }
 
@@ -41,21 +39,21 @@ public class CameraControl: MonoBehaviour
     private void FindAveragePosition ()
     {
         Vector3 averagePos = new Vector3 ();
+        
         int numTargets = 0;
 
-        // Go through all the targets and add their positions together.
         for (int i = 0; i < m_Targets.Length; i++)
+        
         {
-            // If the target isn't active, go on to the next one.
+
             if (!m_Targets[i].gameObject.activeSelf)
                 continue;
 
-            // Add to the average and increment the number of targets in the average.
             averagePos += m_Targets[i].position;
+            
             numTargets++;
         }
 
-        // If there are targets divide the sum of the positions by the number of them to find the average.
         if (numTargets > 0)
             averagePos /= numTargets;
 
